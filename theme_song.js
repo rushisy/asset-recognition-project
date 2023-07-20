c9a1f0906c4734fea2e30e7447a55ff1ac41272f
rushisy <rushisy@gmail.com>
2023-07-19 21:33:54 -0500

    gitignore changes praying

--- theme_song.js
+++ theme_song.js
@@ -1,6 +1,13 @@
-const fast = 'rushi';
-const slow = 'yalamanchili';
+const l = 'left';
+const r = 'right';
+console.log('if you\'re going hard enough ${l}, you\'ll find yourself turning ${r}');
 
-console.log('go ${fast}, not ${slow}');
+displayImage('cars.jpg', 1079, 449)
 
-console.log('go fast ${fast}, not ${slow}');
+function displayImage(src, width, height) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    document.body.appendChild(img);
+   }
--- trains/bin/activate.fish
+++ trains/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rushisy/Desktop/asset recognition project/trains"
+set -gx VIRTUAL_ENV "/Users/rushisy/Desktop/asset-recognition-project/trains"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
